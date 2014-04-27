b22a06e8472b884f7d610e17da2b5f59a1725adb
Jakob Buis <jpabuis@gmail.com>
2014-04-27 22:55:26 +0200

    made reader status messages a little more clear

--- reader_status.php
+++ reader_status.php
@@ -3,7 +3,7 @@
 // Validate context
 $scard = scard_establish_context();
 if (! scard_is_valid_context($scard)) {
-    echo "Context is invalid!\n";
+    echo "Fatal: context is invalid\n";
     exit;
 }
 
@@ -12,16 +12,17 @@ $readers = scard_list_readers($scard);
 
 // Can't connect
 if(!$readers) {
-    echo "Reader not found";
+    echo "Fatal: can't connect to reader";
     exit;
 }
 
 // Too many readers found
-if (count($readers) > 1) {
-    echo "Too many readers: ".count($readers)."\n";
+$count = count($readers);
+if ($count > 1) {
+    echo "Fatal: too many readers ($count). Unsure which one to use\n";
     exit;
 }
 
-// No failures found, report success
-echo "Reader online and ready. All checks passed\n";
+// No failures found, report success 
+echo "Success: reader online and ready. All checks passed\n";
 exit;
