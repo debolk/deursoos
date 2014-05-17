d4d5b2ac15e94e213a1da6f91f630c561f89e749
Jakob Buis <jpabuis@gmail.com>
2014-05-17 10:45:39 +0200

    fixed a bug (missing ;) in authenticate.php and defined usefull constants for valid return codes

--- authenticate.php
+++ authenticate.php
@@ -4,13 +4,23 @@
  * authenticate the card in LDAP
  * open the door if access is granted 
  */
+
+// Define constants for return values of the script
+define('RETURN_ACCEPTED', 0);
+define('RETURN_ERROR',    1);
+define('RETURN_DENIED',   2);
+
+if (! isset($argv[1])) {
+    syslog(LOG_EMERG, 'authenticate.php called without a card ID');
+    exit(RETURN_ERROR);
+}
 $card_id = preg_replace('/[()*\\\\]/','\\\\$0',$argv[1]);
 
 // Connect to ldap
 $ldap = ldap_connect();
 if (!$ldap) {
     syslog(LOG_EMERG, 'Cannot connect to LDAP');
-    exit;
+    exit(RETURN_ERROR);
 }
 
 // Find the card in LDAP
@@ -20,14 +30,14 @@ $card_ldap = ldap_search($ldap, 'dc=bolkhuis,dc=nl', '(&(objectClass=device)(ser
 $count = ldap_count_entries($ldap, $card_ldap);
 if($count > 1) {
 	syslog(LOG_NOTICE, "Multiple owners detected for card $card_id");
-	exit(1);
+	exit(RETURN_DENIED);
 }
 elseif($count == 0)
 {
 	syslog(LOG_NOTICE, "Rejected card $card_id");
     // Card could not be found in LDAP, log to failures.txt
     file_put_contents('failures.txt', date().' '.$card_id);
-	exit(2);
+	exit(RETURN_DENIED);
 }
 
 // Check whether the card has only one owner
@@ -36,11 +46,11 @@ $card_dn = ldap_get_dn($ldap, $card_ldap);
 
 $owner = preg_replace('/^[^,]*,/','',$card_dn);
 $owner_ldap = ldap_search($ldap, $owner, '(objectClass=inetOrgPerson)');
-$count = ldap_count_entries($ldap, $owner_ldap)
+$count = ldap_count_entries($ldap, $owner_ldap);
 if($count != 1)
 {
 	syslog(LOG_NOTICE, "Card $card_id has too many owners ($count)");
-	exit(3);
+	exit(RETURN_DENIED);
 }
 
 // Check for authorisation to use the door
@@ -55,7 +65,9 @@ if(in_array('gosaIntranetAccount', $attributes['objectClass']))
 		exec('/home/deursysteem/open');
 		sleep(1);
 	}
+    exit(RETURN_ACCEPTED);
 }
 else {
 	syslog(LOG_NOTICE, "Denied entry of $owner with card ID $card_id");
+    exit(RETURN_DENIED);
 }
